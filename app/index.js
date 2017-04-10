93e190f4386a57a941a143fa731b1c7d63dceee1
Timur <timur.isakzhanov@gmail.com>
2017-04-10 13:59:13 +0600

    installed fast-levenstein library

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const fileReader = require('./fileReader')
-const matchLines = require('./matcher/baseMatcher')
+const matcher = require('./matcher/baseMatcher')
+
 Promise.all([
 	fileReader().readLinesFromFile('res/input.txt'),
 	fileReader().readLinesFromFile('res/patterns.txt'),
 ]).then((arrays) => {
-	let lines = matchLines(arrays[0], arrays[1])
+	let lines = matcher.matchLines(arrays[0], arrays[1])
 	displayMatchedLines(lines)
 }).catch((error) => {
 	// handle error later
--- app/matcher/baseMatcher.js
+++ app/matcher/baseMatcher.js
@@ -1,20 +1,30 @@
 'use strict'
 
+const baseMatcher = {
 
-const matchLines = (linesArr, patternArr) => {
-	let matchedLines = []
+	matchLines: function(linesArr, patternArr) {
+		let matchedLines = []
 
-	patternArr.forEach((pattern) => {
-		linesArr
-			.filter((line) => {
-				return line == pattern		
-			})
-			.map((line) => {
-				matchedLines.push(line)
-			})
-	})	
+		patternArr.forEach((pattern) => {
+			linesArr
+				.filter((line) => this.comparator(line, pattern))
+				.map((line) => {
+					matchedLines.push(line)
+				})
+		})	
 	
-	return matchedLines
+		return matchedLines
+	}
 }
 
-module.exports = matchLines
+let exactMatcher = Object.create(baseMatcher)
+exactMatcher.comparator = function(line, pattern) {
+	return line == pattern
+}
+
+let containMatcher = Object.create(baseMatcher)
+containMatcher.comparator = function(line, pattern) {
+	return line.includes(pattern)
+}
+
+module.exports = containMatcher
